58df1da456719d201ebc3fed1a2061506aff6494
Gomida <145982843+Gomida05@users.noreply.github.com>
2025-09-11 18:52:00 +0100

    new Updates with new release apk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         applicationId = "com.das.mediaHub"
         minSdk = 24
         targetSdk = 36
-        versionCode = 5
-        versionName = "1.25"
+        versionCode = 6
+        versionName = "1.26"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         flavorDimensions.add("pyVersion")
